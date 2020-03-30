3856ece5753ea50409cc5556c0365c1f38f7b94c
tilda21 <tilda_26@hotmail.com>
2020-03-30 15:56:27 +0100

    run build command

---
+++ client/build/precache-manifest.c303b9c39b53fadf04895d354b74fa46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c610c16f4d50abdfecd75298ce9858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54547d9919a2f3c81c29",
+    "url": "/static/css/main.941ea0ff.chunk.css"
+  },
+  {
+    "revision": "9b3950937d4e7b47fc80",
+    "url": "/static/js/2.ac275b50.chunk.js"
+  },
+  {
+    "revision": "54547d9919a2f3c81c29",
+    "url": "/static/js/main.85fa2ae4.chunk.js"
+  },
+  {
+    "revision": "42d4efa805a127cb095f",
+    "url": "/static/js/runtime-main.fe6f3601.js"
+  }
+]);
